02519e5952cb7107f500c091b8144b82d2b47aec
Eric Kjellman <ekjellman@gmail.com>
2018-01-27 08:56:11 -0600

    TODO cleanup

--- game_app.py
+++ game_app.py
@@ -26,13 +26,13 @@ import subprocess
 #            But: It is inconsistent even with the same log. If you have a
 #                 victory log, and you pass it several times, it seems to work
 #                 eventually?
-# TODO: Have Strength give a small defense buff?
 # TODO: Bug: Renew really seems to have a rounding thing. What's going on?
 # TODO: Have Lucky save you from death once
-# TODO: Add tower refresh item to mysteries trader
 # TODO: Add time reduction item to mysteries trader? Or a time slow buff
 # TODO: When you get five levels of Merchant Warrior, credit card.
 #       This will probably require a "spend gold" method
+# TODO: Make a "has_buff" method on character and get rid of a lot of the
+#       combined_impact stuff
 
 def write_color_text(rtc, string):
   # Takes a wx.richtext.RichTextCtrl and writes my wacky custom color-coded
