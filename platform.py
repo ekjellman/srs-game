2fc77a02fde78af859866a5e3ce4fd5269c20798
Curtis Rueden <ctrueden@wisc.edu>
2018-07-05 08:58:40 -0500

    Add a way to tell when we are in Transcrypt mode
    This will allow for platform-specific case logic.

---
+++ platform.py
@@ -0,0 +1,11 @@
+# NB: Using try/except in this way does not seem to work in Transcrypt.
+# Fortunately, in Transcrypt mode, this variable is defined in srs.html.
+# Whereas in Python mode, this variable is not, and the try/except works.
+try:
+  transcrypt_mode
+except NameError:
+  is_browser = False
+  NL = '\n'
+else:
+  is_browser = True
+  NL = '<br>'
